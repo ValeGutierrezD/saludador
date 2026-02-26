6bb3d9ee979edbfaf666fcec98554bd849468848
Valeria Gutierrez <valeria.gutierrez@ucb.edu.bo>
2026-02-25 23:51:50 -0400

    feat:saludar con hola

--- src/presenter.js
+++ src/presenter.js
@@ -4,7 +4,7 @@ const nombreInput = document.querySelector("#nombre");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => { 
   event.preventDefault();
 
   const nombre = nombreInput.value;
